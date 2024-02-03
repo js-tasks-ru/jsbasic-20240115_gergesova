6eadcbd749ff8795cfc77d3e1ed50ce144c8bf6c
Yuliia Kosterina <gergesova@gmail.com>
2024-02-03 21:43:34 +0300

    extra code has been removed from the tasks 1-2 in 3 module

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,10 +1,4 @@
-let arr = [5, 3, 8, 1];
-
 function filterRange(arr, a, b) {
   return arr.filter(item => a <= item && b >= item);
 }
 
-let filtered = filterRange(arr, 1, 4);
-
-console.log(filtered); // [3,1] (совпадающие значения)
-console.log(arr); // [5,3,8,1] (без изменений)
