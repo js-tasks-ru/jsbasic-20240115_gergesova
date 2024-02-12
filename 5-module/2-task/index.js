94730a4d4d9e73884456a9908aba43f868742448
Yuliia Kosterina <gergesova@gmail.com>
2024-02-13 00:36:29 +0300

    Решения 1-3 задач 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  const textHidden = document.getElementById('text');
+  const button = document.querySelector('.toggle-text-button');
+
+  button.onclick = () => {
+    textHidden.hidden = true;
+  }
+
+  button.onclick = () => {
+    textHidden.hidden = !textHidden.hidden;
+  }
 }
