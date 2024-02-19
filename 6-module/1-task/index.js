70c07ab19d7d8d37057d0d700c2bcaef36966b4a
Yuliia Kosterina <gergesova@gmail.com>
2024-02-19 17:02:34 +0300

    Решения 1-3 задач 6 модуля

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,41 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+
 export default class UserTable {
+
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.rows = rows;
+    this.newTable();
+    this.deleteTr();
+  }
+
+  newTable() {
+    this.elem.innerHTML = `
+<thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>`+
+      this.rows.map(item => `
+      <tr>
+      <td>${item.name}</td>
+      <td>${item.age}</td>
+      <td>${item.salary}</td>
+      <td>${item.city}</td>
+      <td><button>X</button></td>
+    </tr>
+      </tbody>`).join('');
+  }
+
+  deleteTr() {
+    let buttons = Array.from(this.elem.querySelectorAll("button"));
+    buttons.forEach((button) => {
+      button.addEventListener('click', (event) =>
+        event.target.closest("tr").remove());
+    })
   }
 }
